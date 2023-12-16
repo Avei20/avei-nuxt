bc9f1f060dd4c49a48d8b7e1a0d7b1e43162b930
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-12-16 19:53:14 +0700

    Changing entrypoint

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello World. Avei</h1>
+    <h1>This is created by Avei at <a href="https://github.com/Avei20">Avei20</a></h1>
   </div>
 </template>
