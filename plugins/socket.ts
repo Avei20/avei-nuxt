c915a413230673e669f9f3fa2bcf6147da39e9a4
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-12-27 17:56:36 +0700

    fix: Socket not automatically connected to backend

--- plugins/socket.ts
+++ plugins/socket.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig()
 
     const socket = io(config.public.API || 'https://nest.avei.ovh', {
-        autoConnect: false,
+        autoConnect: true,
     })
 
     return {
