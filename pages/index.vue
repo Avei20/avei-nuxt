a17c45a6e9a9550a2f29189cbc08b9ad6e1a7ebf
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-12-27 17:54:45 +0700

    feat: Adding Linkedin, Github and Twitter link into main pages also feature to give feedback to my service

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <VCard class="ma-5">
-        <VCardTitle color="primary" class="text-center font-bold">Avei Project.</VCardTitle>
-        <VCardSubtitle class="text-center">
-            Here are a list of my production work which accessed by my Github account at <a href="https://github.com/Avei20" class="text-white">Avei20</a></VCardSubtitle>
+    <VCard class="ma-5 d-flex flex-column justify-center align-center">
+        <VAvatar image="@/assets/avei.jpg" size="80" class=""/>
+        <VCardTitle color="primary" class="text-center font-bold">I am Avei</VCardTitle>
+        <VCardItem class="">
+            <VBtn class="mx-1" icon="mdi-github" density="compact" href="https://github.cob/Avei20"/>
+            <VBtn class="mx-1" icon="mdi-linkedin" density="compact" href="https://www.linkedin.com/in/avei"/>
+            <VBtn class="mx-1" icon="mdi-twitter" density="compact" href="https://twitter.com/avei__"/>
+        </VCardItem>
+        <VCardText class="text-center py-2">
+            Here are a list of my production work which can be accessed by in my Github account.
+        </VCardText>
         <VCardItem class="w-100">
             <VDivider/>
             <VBtn class="w-100 my-1" color="primary" to="/chat"> Gemini Chat </VBtn>
@@ -10,5 +17,15 @@
             <VBtn class="w-100 my-1" color="primary" href="https://echo.avei.ovh/"> Echo API Swagger Docs  </VBtn>
             <VBtn class="w-100 my-1" color="primary" href="https://elysia.avei.ovh/"> Elysia API Swagger Docs  </VBtn>
         </VCardItem>
+        <VCardText>Fell free to give a feedback and hate comment here. Thanks. Your identity is anonymous. </VCardText>
+        <VTextarea 
+            class="my-1 w-100 mx-2 px-3" 
+            variant="outlined" 
+            density="compact" 
+            label="Feedback"
+            placeholder="Under development hehe. Still working on database to store the feedback. XD">
+            
+        </VTextarea>
+        <VBtn class="mb-4" color="primary">Send</VBtn>
     </VCard>
 </template>
